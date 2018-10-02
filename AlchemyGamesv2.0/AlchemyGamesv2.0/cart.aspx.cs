b7db4d763606a91f673911b734f3a64400c6b55f
JAMES SWART <201470934@student.uj.ac.za>
2018-10-02 23:53:41 +0200

    cart fixes v2.0

--- AlchemyGamesv2.0/AlchemyGamesv2.0/cart.aspx.cs
+++ AlchemyGamesv2.0/AlchemyGamesv2.0/cart.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -14,36 +15,34 @@ namespace AlchemyGamesv2._0
             var db = new AlchemyLinkDataContext();
 
             List<int> items = ShoppingCart.getCartItems();
-            String display = "";
+            StringBuilder display = new StringBuilder();
 
-            for(int i = 0; i < items.Count; i++)
+            for (int i = 0; i < items.Count; i++)
             {
                 Product prod = (from p in db.Products
                                 where p.Id.Equals(items.ElementAt(i))
                                 select p).FirstOrDefault();
 
-                display = "<tr>" + Environment.NewLine;
-                display += "<td class=\"product-name\">" + Environment.NewLine;
-                display += "<div class=\"product-thumbnail\">" + Environment.NewLine;
-                display += "<img src=\"images\\"+ prod.ImageLink +"\" />" + Environment.NewLine;
-                display = "</div>" + Environment.NewLine;
-                display = "<div class=\"product-detail\">" + Environment.NewLine;
-                display = "<h3 class=\"product-title\">"+ prod.Name +"</h3>" + Environment.NewLine;
-                display = "<p>"+ prod.Description +"</p>" + Environment.NewLine;
-                display = "</div>" + Environment.NewLine;
-                display = "</tr>" + Environment.NewLine;
-                display = "<td class=\"product-price\">R"+ prod.Price +"</td>" + Environment.NewLine;
-                display = "<td class=\"product-qty\">" + Environment.NewLine;
-                display = "<select name=\"#\">" + Environment.NewLine;
-                display = "<option value=\"1\">1</option> "+ Environment.NewLine +" <option value=\"2\">2</option> "+ Environment.NewLine +" <option value=\"3\">3</option> " + Environment.NewLine;
-                display = "</select>" + Environment.NewLine;
-                display = "<td class=\"product-total\">"+ prod.Price +"</td>" + Environment.NewLine;
-                display = "<td class=\"action\"><a href=\"#\"><i class=\"fa fa-times\"></i></a></td>" + Environment.NewLine;
-                display = "</tr>" + Environment.NewLine;
+                display.Append("<tr>" + Environment.NewLine);
+                display.Append("<td class=\"product-name\">" + Environment.NewLine);
+                display.Append("<div class=\"product-thumbnail\">" + Environment.NewLine);
+                display.Append("<img src=\"images\\" + prod.ImageLink +"\" />" + Environment.NewLine);
+                display.Append("</div>" + Environment.NewLine);
+                display.Append("<div class=\"product-detail\">" + Environment.NewLine);
+                display.Append("<h3 class=\"product-title\">" + prod.Name +"</h3>" + Environment.NewLine);
+                display.Append("<p>" + prod.Description +"</p>" + Environment.NewLine);
+                display.Append("</div>" + Environment.NewLine);
+                display.Append("</tr>" + Environment.NewLine);
+                display.Append("<td class=\"product-price\">R" + prod.Price +"</td>" + Environment.NewLine);
+                display.Append("<td class=\"product-qty\">" + Environment.NewLine);
+                display.Append("<select name=\"#\">" + Environment.NewLine);
+                display.Append("<option value=\"1\">1</option> " + Environment.NewLine +" <option value=\"2\">2</option> "+ Environment.NewLine +" <option value=\"3\">3</option> " + Environment.NewLine);
+                display.Append("</select>" + Environment.NewLine);
+                display.Append("<td class=\"product-total\">" + prod.Price +"</td>" + Environment.NewLine);
+                display.Append("<td class=\"action\"><a href=\"#\"><i class=\"fa fa-times\"></i></a></td>" + Environment.NewLine);
+                display.Append("</tr>" + Environment.NewLine);
 
-                cartDisplay.InnerHtml += display;
-
-                display = "";
+                cartDisplay.InnerHtml = display.ToString();
             }
         }
     }
